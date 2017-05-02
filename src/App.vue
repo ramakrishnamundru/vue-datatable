d1978beb261aa1b625f439fef6177f341c8de746
Ramakrishna <rama.krishna@lorventindia.com>
2017-05-02 15:25:09 +0530

    refactor

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <div>
+            <datatable></datatable>
+        </div>
+    </div>
+</template>
+<script>
+import Vue from 'vue';
+import DataTable from './DataTable.vue';
+export default {
+    components: {
+        "datatable": DataTable
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
---
+++ webpack.mix.js
@@ -0,0 +1,3 @@
+let mix = require('laravel-mix');
+mix.setPublicPath('./')
+mix.js('src/main.js', 'dist/');
